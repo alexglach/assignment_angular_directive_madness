32f71832e9ad3c5a2a157cba9768b9e1892dc885
jsteenb2 <jonathansteenbergen@gmail.com>
2016-09-23 14:15:14 -0700

    updated quotes

--- js/controllers/quotesCtrl.js
+++ js/controllers/quotesCtrl.js
@@ -1,26 +1,46 @@
 app.controller('QuotesCtrl',
   ['$scope', function($scope){
+    $scope.formData = {};
+    $scope.quotes = {};
+    $scope.idCounter = 0;
 
-    $scope.quotes = [];
+    $scope.processForm = function(newForm){
+      $scope.formData.id = $scope.idCounter;
 
-    $scope.processForm = function(){
-      console.log("process form is called")
-      console.log($scope.formData);
-      $scope.quotes.push($scope.formData);
+      // $scope.quotes.push($scope.formData);
+      $scope.quotes[idCounter] = $scope.formData;
+      $scope.idCounter++;
       $scope.formData = {};
-    }
+    };
 
+    $scope.deleteQuote = function(quote){
+      
 
+    };
   }]
 );
 
 app.directive('quoteForm', [function(){
-
+    function clearForm(scope, element, attributes){
+      element.children("[type='text']").val("");
+    }
 
     return {
       templateUrl: 'js/directives/_quoteForm.html',
       restrict: 'E',
-      scope: true
-    }
+      scope: true,
+      link: clearForm
+    };
   }]
-)
+);
+
+app.directive('quoteRow', [function(){
+    return {
+      templateUrl: 'js/directives/_quote.html',
+      restrict: 'A',
+      scope: {
+        quote: "="
+      }
+    };
+  }]
+);
